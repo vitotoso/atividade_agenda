4b2bf7726a85ff0335fb96fec95cf2a9b4eac4b2
DagobertoBotelho <146292818+DagobertoBotelho@users.noreply.github.com>
2023-10-10 19:50:08 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,18 @@
+def cadastrar_contato(contatos,nome,numero,email):
+    contato{
+        'Nome': nome,
+        'Numero': numero,
+        'Email': email
+    }
+    contatos.append(contato)
+    print("Cliente cadastrado com sucesso!")
+    print("-------------------------------------------")
+    print("\n")
+contatos[]
+
+if escolha == 1:
+
+    nome = str(input("Digite o noem do contato: "))
+    numero = int(input("Digite o noem do contato: "))
+    email = str(input("Digite o noem do contato: "))
+    cadastrar_contato(contatos,nome,numero,email)
